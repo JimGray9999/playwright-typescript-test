32e371eec66072db6421d69474daa2cf905a660f
Jim Gray <jgray8689@gmail.com>
2026-03-30 22:01:23 -0400

    Refactor ticket verification test to improve tag visibility checks and enhance structure; ensure tags are correctly associated with their respective tickets

--- tests/ticket-verification.spec.ts
+++ tests/ticket-verification.spec.ts
@@ -22,10 +22,15 @@ test('ticket verifications', async ({ page }) => {
   // Expect the tags, ticket name, column name to match for each value in the test data
   for (const ticket of test_data.ticket_name_test) {
     const ticketTitle = page.getByRole('heading', { name: ticket.title });
+    const ticketCard = ticketTitle.locator('xpath=..');
+    const ticketsColumn = ticketCard.locator('xpath=..');
+    const columnContainer = ticketsColumn.locator('xpath=..');
 
+    // expects the tag to be inside of the specific ticket it is a part of
     await expect(ticketTitle).toBeVisible();
+    await expect(columnContainer.getByRole('heading', { name: ticket.column })).toBeVisible();
     for (const tag of ticket.tags) {
-      await expect(page.getByText(tag).first()).toBeVisible();
+      await expect(ticketCard.getByText(tag, { exact: true })).toBeVisible();
     }
   }
 });
